b9a861ce0107b92b8cfb19355375b9c32d620da6
Boyquotes <webmaster@linux-live-cd.org>
2016-04-17 18:23:52 +0200

    Ajout listing avec categories

---
+++ api_sortezplus/liste_partenaires_categories.php
@@ -0,0 +1,45 @@
+<?php
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=sortezplbdd;charset=utf8','root','patmo11');
+
+    $req = $dbh->prepare("SELECT * FROM sp_posts INNER JOIN sp_postmeta
+                ON sp_posts.ID = sp_postmeta.post_id
+                WHERE sp_posts.post_type = 'ait-dir-item'
+                AND sp_postmeta.meta_key = '_ait-dir-item';");
+    $req->execute();
+
+$req = $dbh->prepare("SELECT * FROM sortezplbdd.sp_posts INNER JOIN sortezplbdd.sp_postmeta ON sortezplbdd.sp_posts.id = sortezplbdd.sp_postmeta.post_id where sortezplbdd.sp_posts.post_type = 'ait-dir-item' AND sortezplbdd.sp_postmeta.meta_key = '_ait-dir-item';");
+$req->execute();
+$i=0;
+while($ligne = $req->fetch())
+    {
+        $tabAdresse = unserialize($ligne["meta_value"]);
+
+        //id de la sortie;
+        $poi[$i]["id_sortie"]= $ligne['ID'];
+
+        // requete pour recuperer les categories
+        $req2 = $dbh->prepare("SELECT * FROM sortezplbdd.sp_terms INNER JOIN sortezplbdd.sp_term_relationships ON sortezplbdd.sp_terms.term_id = sortezplbdd.sp_term_relationships.term_taxonomy_id where sortezplbdd.sp_term_relationships.object_id =". $ligne['ID']."");
+        $req2->execute();
+        $cat[]='';
+        while($terms = $req2->fetchAll()){
+          //print_r($terms);
+          foreach ($terms as $key => $term) {
+            //print_r($term);
+            $term_id = $term['term_id'];
+            $poi[$i]["categories"][$term_id] = $term['name'];
+          }
+        }
+
+        $poi[$i]["lat"]= $tabAdresse['gpsLatitude'];
+        $poi[$i]["long"]=$tabAdresse['gpsLongitude'];
+        $i++;
+    }
+print_r($poi);
+
+}
+catch(PDOException $e) {
+    echo $e->getMessage();
+}
+ ?>
+
